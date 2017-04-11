6527a62755829349ab31769ac0d485355b465ec0
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-04-11 19:31:41 +0100

    Better deploy action file.

--- actions/1_deploy_honeyPot.js
+++ actions/1_deploy_honeyPot.js
@@ -1,17 +1,27 @@
-const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+// Call with $ truffle exec actions/deploy_honeyPot.js
 
+const HoneyPot = artifacts.require("./HoneyPot.sol");
 const Promise = require('bluebird');
 Promise.promisifyAll(web3.eth, { suffix: "Promise" });
 Promise.promisifyAll(web3.version, { suffix: "Promise" });
-const truffleContract = require("truffle-contract");
+const truffleArtifactor = require("truffle-artifactor");
+const artifactor = new truffleArtifactor(__dirname + "/../build/contracts");
 
-const HoneyPot = truffleContract(require(__dirname + "/../build/contracts/HoneyPot.json"));
-HoneyPot.setProvider(web3.currentProvider);
+const honeyPotJson = HoneyPot.toJSON();
 
-return web3.eth.getAccountsPromise()
-    .then(accounts => HoneyPot.new({
-        from: accounts[0],
-        value: web3.toWei(5),
-        gas: 2000000 }))
-    .then(created => console.log("Created HoneyPot at ", created.address));
+module.exports = function() {
+    return web3.eth.getAccountsPromise()
+        .then(accounts => HoneyPot.new({
+            from: accounts[0],
+            value: web3.toWei(5),
+            gas: 2000000 }))
+        .then(created => {
+            console.log("Created HoneyPot at ", created.address);
+            honeyPotJson.address = created.address;
+            return web3.version.getNetworkPromise();
+        })
+        .then(networkId => {
+            honeyPotJson.network_id = networkId;
+            return artifactor.save(honeyPotJson);
+        });
+};
