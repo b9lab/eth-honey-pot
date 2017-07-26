16970010b44a754245b0ca93b7f5cd28dc7d36a9
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-07-26 12:30:48 +0100

    Additions to tests.

---
+++ test/deployment.js
@@ -0,0 +1,24 @@
+const Promise = require("bluebird");
+const HoneyPot = artifacts.require("./HoneyPot.sol");
+const StealthTransfer = artifacts.require("./StealthTransfer.sol");
+
+contract("Deployment", function(accounts) {
+    before("should promisify web3", function() {
+        if (typeof web3.eth.getAccountsPromise !== "function") {
+            Promise.promisifyAll(web3.eth, { suffix: "Promise" });
+        }
+    });
+
+    it("should have deployed a HoneyPot with 7 Ethers", function() {
+        return HoneyPot.deployed()
+            .then(honeyPot => web3.eth.getBalancePromise(honeyPot.address))
+            .then(balance => assert.strictEqual(balance.toString(10), web3.toWei("7")));
+    });
+
+    it("should have deployed and killed a StealthTransfer", function() {
+        return StealthTransfer.deployed()
+            .then(stealth => web3.eth.getCodePromise(stealth.address))
+            .then(code => assert.strictEqual(code, "0x0"));
+    });
+
+});
---
+++ test/expectSolidityException.js
@@ -0,0 +1,27 @@
+module.exports = function(web3Promisified) {
+    return function (promise, gasToUse) {
+        return promise
+            .then(function (txn) {
+                if (typeof txn === "object") {
+                    // We have a txObject
+                    txn = txn.tx;
+                }
+                return web3Promisified.eth.getTransactionReceiptMined(txn);
+            })
+            .then(function (receipt) {
+                // We are in Geth
+                assert.equal(receipt.gasUsed, gasToUse, "should have used all the gas");
+            })
+            .catch(function (e) {
+                if ((e.message).search("invalid JUMP") > -1||
+                    (e.message).search("out of gas") > -1 ||
+                    (e.message).search("invalid opcode") > -1) {
+                    // We are in TestRPC
+                } else if ((e.message).search("please check your gas amount") > -1) {
+                    // We are in Geth for a deployment
+                } else {
+                    throw e;
+                }
+            });
+    };
+};
